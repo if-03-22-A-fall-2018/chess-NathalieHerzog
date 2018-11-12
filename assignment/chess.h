f0a81329c327ffe0d74c616f210b1f04de554566
NathalieHerzog <nathalieherzog2003@gmail.com>
2018-11-12 13:31:45 +0100

    added structs

--- assignment/chess.h
+++ assignment/chess.h
@@ -10,5 +10,25 @@
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+ #ifndef ___CHESS_H
+ #define ___CHESS_H
 
-typedef struct ChessSquare ChessBoard[8][8]
+typedef struct ChessSquare ChessBoard[8][8];
+
+static struct ChessPiece white_pawn = {struct PieceColor, struct PieceType}; //??
+static struct ChessPiece white_king = {struct PieceColor, struct PieceType};
+static struct ChessPiece white_pawn = {struct PieceColor, struct PieceType};
+static struct ChessPiece white_rook = {struct PieceColor, struct PieceType};
+static struct ChessPiece white_knight = {struct PieceColor, struct PieceType};
+static struct ChessPiece white_bishop = {struct PieceColor, struct PieceType};
+static struct ChessPiece white_queen = {struct PieceColor, struct PieceType};
+
+static struct ChessPiece black_pawn = {struct PieceColor, struct PieceType};
+static struct ChessPiece black_rook = {struct PieceColor, struct PieceType};
+static struct ChessPiece black_knight = {struct PieceColor, struct PieceType};
+static struct ChessPiece black_bishop = {struct PieceColor, struct PieceType};
+static struct ChessPiece black_queen = {struct PieceColor, struct PieceType};
+static struct ChessPiece black_king = {struct PieceColor, struct PieceType};
+
+
+#endif
