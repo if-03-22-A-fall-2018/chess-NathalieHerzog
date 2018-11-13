f07d0c31efb866cdcfdcadc68ec8b940ff6f441f
NathalieHerzog <nathalieherzog2003@gmail.com>
2018-11-13 10:38:52 +0100

    building works

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -10,3 +10,102 @@
  * Implementation of basic chess functions.
  * ----------------------------------------------------------
  */
+
+  #include <stdlib.h>
+ #include "chess.h"
+ #include "general.h"
+
+  bool is_square_ok (File file, Rank rank)
+  {
+    return (file <= 'h') && (file >= 'a') && (rank <= 8) && (rank > 0);
+  }
+
+int 	nr (Rank rank)
+  {
+    return 0;
+  }
+
+int 	nf (File file)
+{
+  return 0;
+}
+
+bool 	is_move_from_base_line (enum PieceColor color, Rank rank)
+{
+  return false;
+}
+
+bool is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type)
+{
+ return (pc.color == color) && (pc.type == type);
+}
+
+void 	init_chess_board (ChessBoard chess_board)
+{
+
+}
+
+struct ChessSquare * 	get_square (ChessBoard chess_board, File file, Rank rank)
+{
+  return 0;
+}
+
+bool 	is_square_occupied (ChessBoard chess_board, File file, Rank rank)
+{
+  return false;
+}
+
+bool 	add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece)
+{
+  return false;
+}
+
+struct ChessPiece get_piece (ChessBoard chess_board, File file, Rank rank)
+{
+  return chess_board[file - 1][rank - 1].piece;
+}
+
+void 	setup_chess_board (ChessBoard chess_board)
+{
+
+}
+
+bool 	remove_piece (ChessBoard chess_board, File file, Rank rank)
+{
+  return false;
+}
+
+bool 	squares_share_file (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+{
+  return false;
+}
+
+bool 	squares_share_rank (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+{
+  return false;
+}
+
+bool 	squares_share_diagonal (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+{
+  return false;
+}
+
+bool 	squares_share_knights_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+{
+  return false;
+}
+
+bool 	squares_share_pawns_move (enum PieceColor color, enum MoveType move, File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+{
+  return false;
+}
+
+bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+{
+  return false;
+}
+
+bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+{
+  return false;
+}
--- assignment/chess.h
+++ assignment/chess.h
@@ -10,25 +10,44 @@
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+
  #ifndef ___CHESS_H
  #define ___CHESS_H
 
-typedef struct ChessSquare ChessBoard[8][8];
+  enum PieceType { NoPiece, Pawn, Rook, Knight, Bishop, Queen, King };
+  enum PieceColor { White, Black };
+	enum MoveType { NormalMove, CaptureMove };
 
-static struct ChessPiece white_pawn = {struct PieceColor, struct PieceType};
-static struct ChessPiece white_king = {struct PieceColor, struct PieceType};
-static struct ChessPiece white_pawn = {struct PieceColor, struct PieceType};
-static struct ChessPiece white_rook = {struct PieceColor, struct PieceType};
-static struct ChessPiece white_knight = {struct PieceColor, struct PieceType};
-static struct ChessPiece white_bishop = {struct PieceColor, struct PieceType};
-static struct ChessPiece white_queen = {struct PieceColor, struct PieceType};
+  struct ChessPiece
+  {
+    enum PieceColor color;
+    enum PieceType type;
+  };
 
-static struct ChessPiece black_pawn = {struct PieceColor, struct PieceType};
-static struct ChessPiece black_rook = {struct PieceColor, struct PieceType};
-static struct ChessPiece black_knight = {struct PieceColor, struct PieceType};
-static struct ChessPiece black_bishop = {struct PieceColor, struct PieceType};
-static struct ChessPiece black_queen = {struct PieceColor, struct PieceType};
-static struct ChessPiece black_king = {struct PieceColor, struct PieceType};
+  struct ChessSquare
+  {
+    bool is_occupied;
+    struct ChessPiece piece;
+  };
 
+  typedef struct ChessSquare 	ChessBoard [8][8];
+  typedef char 	File;
+  typedef int 	Rank;
 
-#endif
+  bool 	is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type);
+  void 	init_chess_board (ChessBoard chess_board);
+  struct ChessSquare * 	get_square (ChessBoard chess_board, File file, Rank rank);
+  bool 	is_square_occupied (ChessBoard chess_board, File file, Rank rank);
+  bool 	add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece);
+  struct ChessPiece 	get_piece (ChessBoard chess_board, File file, Rank rank);
+  void 	setup_chess_board (ChessBoard chess_board);
+  bool 	remove_piece (ChessBoard chess_board, File file, Rank rank);
+  bool 	squares_share_file (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+  bool 	squares_share_rank (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+  bool 	squares_share_diagonal (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+  bool 	squares_share_knights_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+  bool 	squares_share_pawns_move (enum PieceColor color, enum MoveType move, File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+  bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+  bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r);
+
+ #endif
